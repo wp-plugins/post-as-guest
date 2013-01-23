daf297711a04b91163bfa9d26a4af67fdde01db2
PowieT <PowieT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-23 07:44:52 +0000

    0.9.2 - select category
    git-svn-id: https://plugins.svn.wordpress.org/post-as-guest/tags/0.9.2@657258 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pag_settings.php
@@ -0,0 +1,101 @@
+<div class="wrap">
+<div class="icon32" id="icon-options-general"></div>
+<h2><?php _e('Post as guest Settings', 'pag') ?></h2>
+<form method="post" action="options.php">
+<?php
+settings_fields( 'pag-settings' );
+if  ( get_option('postfield-legend') == '' ) {
+	update_option('postfield-legend',__('Post Content','pag'));
+}
+?>
+<div id="poststuff">
+<div class="postbox">
+<h3><?php _e('Form Settings', 'pag') ?></h3>
+	<div class="inside">
+    <table class="form-table">
+        <tr valign="top">
+        	<th scope="row"><?php _e('Post Field Rows', 'pag') ?></th>
+        	<td><input type="text" size="10" name="postfield-rows" value="<?php echo get_option('postfield-rows'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+        	<th scope="row"><?php _e('Post Field Legend', 'pag') ?></th>
+        	<td><input type="text" size="50" name="postfield-legend" value="<?php echo get_option('postfield-legend'); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+	        <th scope="row"><?php _e('User can select Category', 'pag') ?></th>
+    	    <td><input type="checkbox" name="category-select" id="category-select" value="1" <?php checked(get_option('category-select'), 1); ?> /></td>
+        </tr>
+
+    </table>
+    </div>
+</div>
+
+<div class="postbox">
+<h3><?php _e('Guest post settings', 'pag') ?></h3>
+	<div class="inside">
+    <table class="form-table">
+        <tr valign="top">
+        	<th scope="row"><?php _e('Add before post', 'pag') ?></th>
+        	<td><textarea rows="3" name="prepost-code" style="width:100%;"><?php echo get_option('prepost-code'); ?></textarea></td>
+        </tr>
+        <tr valign="top">
+        	<th scope="row"><?php _e('Add after post', 'pag') ?></th>
+        	<td><textarea rows="3" name="afterpost-code" style="width:100%;"><?php echo get_option('afterpost-code'); ?></textarea></td>
+        </tr>
+        <tr valign="top">
+        	<th scope="row"><?php _e('Message after post', 'pag') ?></th>
+        	<td><textarea rows="2" name="after-post-msg" style="width:100%;"><?php echo get_option('after-post-msg'); ?></textarea></td>
+        </tr>
+
+    </table>
+    </div>
+</div>
+
+</div>
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</form>
+<br />
+
+<div id="poststuff">
+<div class="postbox">
+<h3><?php _e('Post as guest Usage', 'pag') ?></h3>
+	<div class="inside">
+		<?php _e('Simple use a shortcode on a page of your choice or create a new page and insert the following shortcode: <code>[post-as-guest]</code> to show the guest form.', 'pag') ?>
+    </div>
+</div>
+
+<div class="postbox">
+<h3><?php _e('About', 'pag') ?></h3>
+	<div class="inside" style="overflow:auto">
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- www -->
+		WWW: <a href="http://www.powie.de">powie.de</a>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- twitter -->
+		<a href="https://twitter.com/PowieT" class="twitter-follow-button" data-show-count="false" data-lang="de">@PowieT folgen</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- fb -->
+		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpowiede&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:350px; height:35px;" allowTransparency="true"></iframe>
+		</div>
+
+		<div style="float:left;margin-right: 10px; display:inline;">
+			<div class="g-plusone" data-size="small" data-href="http://www.powie.de"></div>
+			<script type="text/javascript">
+			  (function() {
+			    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			    po.src = 'https://apis.google.com/js/plusone.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+			  })();
+			</script>
+		</div>
+
+    </div>
+</div>
+
+</div>
+
+</div>
---
+++ post-as-guest.php
@@ -0,0 +1,190 @@
+<?php
+/*
+Plugin Name: Post As Guest
+Plugin URI: http://www.powie.de/wordpress/post-as-guest/
+Description: Post as Guest - Creates a form (shortcode) to a page to allow guests to post
+Version: 0.9.2
+License: GPLv2
+Author: Thomas Ehrhardt
+Author URI: http://www.powie.de
+*/
+
+//Define some stuff
+define( 'PAG_PLUGIN_DIR', dirname( plugin_basename( __FILE__ ) ) );
+define( 'PAG_PLUGIN_URL', plugins_url( dirname( plugin_basename( __FILE__ ) ) ) );
+load_plugin_textdomain( 'pag', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+//create custom plugin settings menu
+add_action('admin_menu', 'pag_create_menu');
+add_action('admin_init', 'pag_register_settings' );
+//add_action('wp_head', 'plinks_websnapr_header');
+//Shortcode
+add_shortcode('post-as-guest', 'pag_shortcode');
+//Hook for Activation
+register_activation_hook( __FILE__, 'pag_activate' );
+//Hook for Deactivation
+register_deactivation_hook( __FILE__, 'pag_deactivate' );
+
+//PAG Ajax Javascripts Admin
+add_action( 'admin_enqueue_scripts', 'pagjs' );
+function pagjs(){
+	wp_enqueue_script( 'pag-js', PAG_PLUGIN_URL.'/pag.js', false );
+}
+
+//Nonces!!! nont nonsens :)
+add_action('init','pagnonces_create');
+function pagnonces_create(){
+	$pagnonce = wp_create_nonce('pagnonce');
+}
+
+//PAG JS Frontend
+// thanks to http://www.garyc40.com/2010/03/5-tips-for-using-ajax-in-wordpress/
+// embed the javascript file that makes the AJAX request
+
+wp_enqueue_script( 'pag-ajax-request', plugin_dir_url( __FILE__ ) . '/pagfe.js', array( 'jquery' ) );
+// declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
+wp_localize_script( 'pag-ajax-request', 'PagAjax',
+	array(  'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			'enter_title' => __('Please enter title', 'pag'),
+			'enter_content' => __('Please enter content', 'pag' )
+	)
+);
+
+//Create Menus
+function pag_create_menu() {
+	// create PAG menu page
+	add_menu_page( __('Post as Guest','pag'), __('Post as Guest','pag'), 8, PAG_PLUGIN_DIR.'/pag_review.php','',PAG_PLUGIN_URL.'/images/pag.png');
+	add_submenu_page( PAG_PLUGIN_DIR.'/pag_review.php', __('Settings','pag'), __('Settings','pag'), 8, PAG_PLUGIN_DIR.'/pag_settings.php');
+}
+
+function pag_register_settings() {
+	//register settings
+	register_setting( 'pag-settings', 'postfield-rows', 'intval' );		//Zeilen Textarea
+	register_setting( 'pag-settings', 'postfield-legend');				//Bezeichnung
+	register_setting( 'pag-settings', 'prepost-code');
+	register_setting( 'pag-settings', 'afterpost-code' );
+	register_setting( 'pag-settings', 'after-post-msg' );
+	register_setting( 'pag-settings', 'category-select' );				//Auswahl der Kategorie gestatten
+}
+
+function pag_shortcode( $atts ) {
+	//var_Dump($atts);
+	/*
+	extract( shortcode_atts( array(
+		'foo' => 'something',
+		'bar' => 'something else',
+	), $atts ) );
+	return "Hallo -> foo = {$foo}";
+	*/
+	$sc = '<!-- post-as-guest -->';
+	$sc.= '<div id="pag_form"><form method="post" id="pag" action="">
+			<input type="hidden" name="action" value="pag_post" />';
+    $sc.= wp_nonce_field( 'pagnonce', 'post_nonce', true, false );
+	$sc.= '<legend>'.__('Title', 'pag').'</legend>
+        	 <input type="text" size="50" name="pagtitle" id="pagtitle" />
+        	<legend>'.get_option('postfield-legend').'</legend>
+        	 <textarea rows="'.get_option('postfield-rows').'" name="pagcontent" id="pagcontent" style="width:100%;"></textarea>';
+	if (get_option('category-select') == 1) {
+		$sc.='<legend>'.__('Category', 'pag').'</legend>
+			   <select id="categoryid" name="categoryid">
+					<option value="">'.__('-- Please Select Category','pag').'</option>';
+						$categories = get_categories('hierarchical=0&hide_empty=0');
+						foreach($categories as $category)	{
+							$selected = (is_category($category->cat_ID)) ? 'selected' : '';
+							$sc.='<option '.$selected.' value="'.$category->cat_ID.'">'.$category->cat_name.'</option>';
+						}
+		$sc.='</select><br />';
+	}
+    $sc.='   <input type="submit" id="pagsubmit" name="pagsubmit" value="'.__("Send In", 'pag').'" /></form>
+		   </div>';
+	$sc.='<!-- /post-as-guest -->';
+	return $sc;
+}
+
+//Activate
+function pag_activate() {
+	// do not generate any output here
+	add_option('postfield-rows',5);
+	add_option('after-post-msg', __('Thanks for your post. We will review your post befor publication.','pag'));
+}
+
+//Deactivate
+function pag_deactivate() {
+	// do not generate any output here
+}
+
+//Post as Guest - get count of pages waiting for review
+function pag_get_posts_wait_count(){
+	$args = array( 'post_type' => 'post', 'post_status' => 'review', 'numberposts' => 100 );
+	$data =get_posts($args);
+	return(count($data));
+}
+
+function pag_get_posts_wait(){
+	$args = array( 'post_type' => 'post', 'post_status' => 'pending', 'orderby' => 'post_date', 'order' => 'DESC',
+					'numberposts' => 100 );
+	return get_posts($args);
+}
+
+//Ajax Functions Backend!
+// Post Preview
+add_action('wp_ajax_pag_post_preview', 'pag_post_preview');
+function pag_post_preview(){
+	$id = intval($_POST['id']);
+	$post = get_post($id);
+	$content =  apply_filters('the_content', $post->post_content);
+	$output = "<tr class=\"pag_preview\" id=\"preview-{$post->ID}\">";
+	$output.= "    <td colspan=\"7\">$content</td>\n";
+	$output.= "</tr>";
+	echo $output;
+	die();
+}
+//Remove Post
+add_action('wp_ajax_pag_post_remove', 'pag_post_remove');
+function pag_post_remove(){
+	$id = intval($_POST['id']);
+	wp_delete_post($id);
+	$response = json_encode( array( 'success' => true ) );
+	header( "Content-Type: application/json" );
+	echo $response;
+	die();
+}
+//Approve Post
+add_action('wp_ajax_pag_post_approve', 'pag_post_approve');
+function pag_post_approve(){
+	$id = intval($_POST['id']);
+	wp_publish_post($id);
+	$response = json_encode( array( 'success' => true ) );
+	header( "Content-Type: application/json" );
+	echo $response;
+	die();
+}
+
+//Ajax Function Frontend
+add_action('wp_ajax_pag_post', 'pag_post');
+add_action('wp_ajax_nopriv_pag_post', 'pag_post' );
+function pag_post(){
+	//ccheck nonce
+	if (! wp_verify_nonce($_POST['post_nonce'], 'pagnonce') ) die('Security check');
+	$content = trim(get_option('prepost-code')).trim($_POST['pagcontent']).trim(get_option('afterpost-code'));
+	if (get_option('category-select') == 1) {
+		$post = array(  'post_title' => $_POST['pagtitle'],
+						'post_content' => $content ,
+						'post_type' => 'post',
+						'post_category' => array(intval($_POST['categoryid'])),
+						'post_status' => 'pending');
+	} else {
+		$post = array(  'post_title' => $_POST['pagtitle'],
+						'post_content' => $content ,
+						'post_type' => 'post',
+						'post_status' => 'pending');
+	}
+	$id = wp_insert_post( $post, $wp_error );
+	$response = json_encode( array( 'success' => true ,
+								    'msg' => get_option('after-post-msg') ) );
+	header( "Content-Type: application/json" );
+	echo $response;
+	//var_dump($_POST);
+	die();
+}
+?>
